e07f44e0f2c7dc692aba74fca437463ff54c823c
Cem GEÇGEL <gecgelcem@outlook.com>
2022-02-17 23:51:57 +0300

    Carried Examples to Doc

--- src/timer.rs
+++ src/timer.rs
@@ -2,19 +2,48 @@ use crate::{now::Now, Sec};
 use std::ops::{Add, AddAssign, Sub};
 
 /// Finds time relative to the moment it is created.
+///
+/// # Example
+///
+/// ```
+/// use std::time::{Duration, Instant};
+/// use min_timer::{now::Std, Sec, Timer};
+///
+/// let now = Std::new();
+/// let timer = Timer::new(&now);
+/// let duration = 5.0 * Sec::MILLI;
+///
+/// // For stopping an infinite loop if the test fails.
+/// let start = Instant::now();
+/// let cap = Duration::from(duration + Sec::MILLI);
+///
+/// while timer < duration {
+///     if start.elapsed() > cap {
+///         unreachable!("Passed {}!", duration);
+///     }
+/// }
+///
+/// let tolerance = 100.0 * Sec::MICRO;
+/// assert!(timer - duration < tolerance);
+/// ```
 #[derive(Debug)]
-pub struct Timer<'a, T>
-where
-    T: Now,
-{
+pub struct Timer<'a, T: Now> {
     start: Sec,
     now: &'a T,
 }
 
-impl<'a, T> Timer<'a, T>
-where
-    T: Now,
-{
+impl<'a, T: Now> Clone for Timer<'a, T> {
+    fn clone(&self) -> Self {
+        Self {
+            start: self.start,
+            now: self.now,
+        }
+    }
+}
+
+impl<'a, T: Now> Copy for Timer<'a, T> {}
+
+impl<'a, T: Now> Timer<'a, T> {
     /// Creates starting from this moment.
     pub fn new(now: &'a T) -> Self {
         Self {
@@ -29,10 +58,7 @@ where
     }
 }
 
-impl<'a, T> Add<Sec> for Timer<'a, T>
-where
-    T: Now,
-{
+impl<'a, T: Now> Add<Sec> for Timer<'a, T> {
     type Output = Self;
 
     fn add(self, rhs: Sec) -> Self::Output {
@@ -43,19 +69,13 @@ where
     }
 }
 
-impl<'a, T> AddAssign<Sec> for Timer<'a, T>
-where
-    T: Now,
-{
+impl<'a, T: Now> AddAssign<Sec> for Timer<'a, T> {
     fn add_assign(&mut self, rhs: Sec) {
         self.start += rhs;
     }
 }
 
-impl<'a, T> Sub<Sec> for Timer<'a, T>
-where
-    T: Now,
-{
+impl<'a, T: Now> Sub<Sec> for Timer<'a, T> {
     type Output = Sec;
 
     fn sub(self, rhs: Sec) -> Self::Output {
@@ -63,10 +83,7 @@ where
     }
 }
 
-impl<'a, T> Sub<Timer<'a, T>> for Sec
-where
-    T: Now,
-{
+impl<'a, T: Now> Sub<Timer<'a, T>> for Sec {
     type Output = Sec;
 
     fn sub(self, rhs: Timer<'a, T>) -> Self::Output {
@@ -74,42 +91,14 @@ where
     }
 }
 
-impl<'a, T> PartialEq<Sec> for Timer<'a, T>
-where
-    T: Now,
-{
+impl<'a, T: Now> PartialEq<Sec> for Timer<'a, T> {
     fn eq(&self, other: &Sec) -> bool {
         self.elapsed() == *other
     }
 }
 
-impl<'a, T> PartialOrd<Sec> for Timer<'a, T>
-where
-    T: Now,
-{
+impl<'a, T: Now> PartialOrd<Sec> for Timer<'a, T> {
     fn partial_cmp(&self, other: &Sec) -> Option<std::cmp::Ordering> {
         self.elapsed().partial_cmp(other)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::time::{Duration, Instant};
-
-    use crate::{now::Std, Sec, Timer};
-
-    #[test]
-    fn waiting_loop() {
-        let clk = Std::new();
-        let timer = Timer::new(&clk);
-        let duration = 5.0 * Sec::MILLI;
-        let start = Instant::now(); // For limiting the waiting.
-        let cap = Duration::from(duration + Sec::MILLI);
-        while timer < duration {
-            if start.elapsed() > cap {
-                unreachable!("Passed {}!", duration);
-            }
-        }
-        assert!(timer - duration < Sec::MICRO);
     }
 }
